bff228176cc501bef1c2842a5de2d9d78dd1b7be
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-02-11 05:00:33 +0700

    modal add book to html and array individual remove button only remove from html

--- script.js
+++ script.js
@@ -22,20 +22,58 @@ const body = document.querySelector("body");
 
 function pushToPara(item) {
   const para = document.createElement("p");
-  para.textContent = `Author: ${item.author} Title: ${item.title} Pages: ${item.pages} Read: ${item.read}`;
+  para.textContent = `Author: ${item.author} Title: ${item.title} Pages: ${item.pages} Read: ${item.read} `;
   body.appendChild(para);
+
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
+  para.appendChild(removeBtn);
+
+  removeBtn.addEventListener("click", () => {
+    body.removeChild(para);
+    body.removeChild(removeBtn);
+  });
 }
 
 myLibrary.forEach((item) => {
   pushToPara(item);
 });
 
-const btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-  const author = prompt("Author");
-  const title = prompt("Title");
-  const pages = prompt("Pages");
-  const read = prompt("Read");
-  addBookToLibrary(author, title, pages, read);
-  pushToPara(myLibrary[myLibrary.length - 1]);
+const addBookBtn = document.querySelector(".js-add-book-btn");
+// addBookBtn.addEventListener("click", () => {
+//   const author = prompt("Author");
+//   const title = prompt("Title");
+//   const pages = prompt("Pages");
+//   const read = prompt("Read");
+//   addBookToLibrary(author, title, pages, read);
+//   pushToPara(myLibrary[myLibrary.length - 1]);
+// });
+const modal = document.querySelector("dialog");
+addBookBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+const dialogSubmitBookBtn = document.querySelector(
+  ".js-dialog-submit-book-btn"
+);
+const form = document.querySelector("form");
+
+const author = modal.querySelector("#author");
+const title = modal.querySelector("#title");
+const pages = modal.querySelector("#pages");
+const read = modal.querySelector("#read");
+
+dialogSubmitBookBtn.addEventListener("click", (e) => {
+  // e.preventDefault();
+  if (author.value && title.value && pages.value && read.value) {
+    addBookToLibrary(author.value, title.value, pages.value, read.value);
+    pushToPara(myLibrary[myLibrary.length - 1]);
+    form.reset();
+  }
+});
+
+const dialogCloseBtn = document.querySelector(".js-dialog-close-btn");
+
+dialogCloseBtn.addEventListener("click", () => {
+  modal.close();
 });
